2c00981aa0e9b1c22b6404fa6c6d29ba5bff6f6c
Jacek Gołębiewski <jacekg1993@gmail.com>
2019-06-25 19:20:22 +0200

    CharToMatrixService master update

--- src/main/java/pl/raspberry/box/app/examples/confitura/ConfituraApp.java
+++ src/main/java/pl/raspberry/box/app/examples/confitura/ConfituraApp.java
@@ -3,8 +3,8 @@ package pl.raspberry.box.app.examples.confitura;
 import lombok.extern.slf4j.Slf4j;
 import pl.raspberry.box.app.RaspberryBoxApplication;
 import pl.raspberry.box.core.model.response.button.Button;
-import pl.raspberry.box.core.service.ScreenService;
 import pl.raspberry.box.core.service.console.ConsoleInputService;
+import pl.raspberry.box.core.service.screen.ScreenService;
 
 /**
  * Szablon aplikacji confitura2019
